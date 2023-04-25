09ba506dc106816a7dad8c79a6e71e0a8563eca1
Aryelson Santos <aryelson@gmail.com>
2023-04-25 16:34:05 -0300

    Add Contato Controller

---
+++ AgendaPessoal/Apresentacao/Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+﻿using Aplicacao.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apresentacao.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly IContatoService _contatoService;
+
+        public ContatoController(IContatoService contatoService)
+        {
+            _contatoService = contatoService;
+        }
+
+        public async Task<IActionResult> ListaContatosAsync()
+        {
+            return Ok(await _contatoService.BuscaContatosAsync());
+        }
+
+        public async Task<IActionResult> ListaContatosPorNomeAsync(string nome)
+        {
+            return Ok(await _contatoService.BuscaContatosPorNomeAsync(nome));
+        }
+    }
+}
